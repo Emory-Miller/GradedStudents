157bc6aa7add3d6fb1222e0c3004529f6e3aefc1
Emory <emory@Zipcoder-Emorys-MacBook-Pro.local>
2022-11-09 13:28:48 -0500

    Working on the Student Class

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,53 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, ArrayList<Double> examScores){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void getExamScores() {
+        System.out.println(examScores);
+    }
+
+    public void addExamScore(double examScore){
+        examScores.add(examScore);
+    }
+
+    public void setExamScore(int examNumber, double examScore) {
+        examScores.add(examNumber, examScore);
+    }
+
+    public void getAverageExamScore(){
+        double sumExamScore = 0.0;
+        for (double value : examScores){
+            sumExamScore += value;
+        }
+        System.out.println(sumExamScore/examScores.size());
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,18 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StudentTest {
 
+    @Test
+    public void constructorTest(){
+        String firstName = "Emory";
+        String lastName = "Miller";
+        ArrayList<Double> examScores = new ArrayList<>();
+        double[] examScoresArray = {95, 100, 95, 100};
+        examScores = Arrays.asList(examScoresArray).toArray();
+    }
 }
